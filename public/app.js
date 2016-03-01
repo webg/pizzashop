12b48d38f6fdbfca06519e6fe5918591e65ffb07
webg <diyhouse@yandex.ru>
2016-03-02 00:26:34 +0300

    Add cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -9,7 +9,24 @@ function add_to_cart(id)
 		
 		window.localStorage.setItem(key, x);
 		
-			
+		alert('Вы заказали:' + cart_get_number_of_items());	
 	}
 
 
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+	
+	for(var i=0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if (key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+}
