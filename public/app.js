f3e0b0ecc17c09b7845b1642ccd2bc0acec663db
webg <diyhouse@yandex.ru>
2016-03-04 22:41:50 +0300

    Update orders button

--- public/app.js
+++ public/app.js
@@ -10,13 +10,38 @@ function add_to_cart(id)
 		
 		window.localStorage.setItem(key, x);
 		
-		update_orders_input();	
+		update_orders_input();
+		update_orders_button();	
 	}
 
 function update_orders_input()
 {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_items() + ')';
+	$('#orders_button').val(text);
+}
+
+function cart_get_items()
+{
+	var cnt = 0;
+	
+	for(var i=0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if (key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value*1;
+		}
+	}
+
+	return cnt;
 }
 function cart_get_orders()
 {
